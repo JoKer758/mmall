60eb7161eb4d4cd43be5d75ae2fdab3a6382b52c
joker <supliyue@163.com>
2018-12-11 09:39:17 +0800

    deploy

---
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -0,0 +1,27 @@
+package com.mmall.controller.portal;
+
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+import com.mmall.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+/**
+ * Created by joker on 2018/12/9.
+ */
+@Controller
+@RequestMapping("/user/")
+public class UserController {
+    @Autowired
+    private IUserService iUserService;
+
+    @RequestMapping(value="get_information.do", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<User> get_information(Integer userId){
+        return iUserService.getInformation(userId);
+    }
+}
---
+++ src/main/java/com/mmall/service/IUserService.java
@@ -0,0 +1,14 @@
+package com.mmall.service;
+
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+
+/**
+ * Created by joker on 2018/12/10.
+ */
+public interface IUserService {
+    ServerResponse<User> login(String username, String password);
+    ServerResponse<User> getInformation(Integer userId);
+
+
+}
